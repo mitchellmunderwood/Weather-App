c980d2980852aa134c129c5b2219d3d3bd196c5c
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-06-24 16:11:39 -0400

    Got the data stored to local object

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
             var searchText = cityInput.val();
             searchArray.push(searchText);
             renderList();
-            // weatherPull(searchText);
+            weatherPull(searchText);
         }
     });
 
@@ -19,7 +19,7 @@ $(document).ready(function () {
         var searchText = cityInput.val();
         searchArray.push(searchText);
         renderList();
-        // weatherPull(searchText);
+        weatherPull(searchText);
     });
 
     function renderList() {
@@ -31,6 +31,47 @@ $(document).ready(function () {
             }
             searchList.prepend(new_par);
         }
+    }
+
+    function weatherPull(searchText) {
+        var APIKey = "166a433c57516f51dfab1f7edaed8413";
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "q=" + searchText + "&appid=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log("full response", response);
+            weatherLog(response);
+            // weatherRender();
+        });
+    }
+
+    function weatherLog(response) {
+        var indices = [0, 8, 16, 24, 32, 39]
+
+        for (i = 0; i < indices.length; i++) {
+            var today = moment();
+            var conditions = {};
+            // console.log(response.list[0])
+            // console.log(indices.length);
+            conditions.sky = response.list[indices[i]].weather[0].description;
+            var tempC = response.list[indices[i]].main.temp;
+            conditions.temp = Math.round(((tempC - 273.15) * 9 / 5) + 32);
+            conditions.humidity = response.list[indices[i]].main.humidity;
+            conditions.wind = response.list[indices[i]].wind.speed;
+
+            var date = datePlusDay(today, i);
+            weatherResults[date] = conditions;
+        }
+        console.log("logged info", weatherResults);
+    }
+
+    function datePlusDay(date, index) {
+        var new_date = date.add(index, 'day');
+        var month = new_date.month();
+        var day = new_date.date();
+        var year = new_date.year();
+        return "" + (month + 1) + "/" + day + "/" + year;
     }
 
     renderList();
